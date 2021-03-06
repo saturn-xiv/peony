41dc274fa899403b3fb3e11511bdbfc2d2e93699
Jeremy Zheng <jitang.zheng@gmail.com>
2021-03-05 16:58:19 -0800

    :wrench: add docker package

--- docker/docker.sh
+++ docker/docker.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export VERSION=$(date "+%4Y%m%d%H%M%S")
+
 buildah pull ubuntu:latest
 
 # https://wiki.ubuntu.com/Releases
@@ -15,6 +17,7 @@ for i in "${features[@]}"
 do
     buildah pull ubuntu:$i
     buildah bud --build-arg CODE_VERSION=$i -t peony-$i .
+    podman save > peony-$VERSION.tar peony
 done
 
 echo 'done.'
