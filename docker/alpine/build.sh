8b7663ad09ea28a3b0354b21ac0a462039e4d49a
Jeremy Zheng <jitang.zheng@gmail.com>
2021-03-14 09:38:25 -0700

    :wrench: using rust & nodejs

--- docker/alpine/build.sh
+++ docker/alpine/build.sh
@@ -3,10 +3,11 @@
 set -e
 
 export VERSION=$(date "+%4Y%m%d%H%M%S")
+export CODE=peony-alpine
 
 buildah pull alpine:latest
-buildah bud --layers -t peony-alpine-amd64 .
-podman save > peony-$i-$VERSION.tar peony-alpine-amd64
+buildah bud --layers -t $CODE-amd64 .
+# podman save > peony-$i-$VERSION.tar $CODE-amd64
 
 echo 'done.'
 exit 0
