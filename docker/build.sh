8281baf709e475f245e5f7f967e97f18a92b3ff2
Jeremy Zheng <jitang.zheng@gmail.com>
2021-03-21 00:57:58 +0000

    :wrench: add ansible setup

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,7 @@ set -e
 export VERSION=$(date "+%4Y%m%d%H%M%S")
 
 buildah pull ubuntu:xenial
-buildah bud --layers --build-arg -t peony .
+buildah bud --layers -t peony .
 podman save -o peony-$VERSION.tar peony
 
 echo 'done.'
--- docker/clean.sh
+++ docker/clean.sh
@@ -11,26 +11,26 @@ fi
 
 mkdir -p $WORKSPACE/build/amd64-debug
 cd $WORKSPACE/build/amd64-debug
-conan install $WORKSPACE --build=missing --profile=$WORKSPACE/profiles/amd64
+conan install $WORKSPACE --build=missing --profile=/opt/conan/profiles/amd64
 cmake $WORKSPACE
 make 
 
 mkdir -p $WORKSPACE/build/amd64-release
 cd $WORKSPACE/build/amd64-release
-conan install $WORKSPACE --build=missing --profile=$WORKSPACE/profiles/amd64
+conan install $WORKSPACE --build=missing --profile=/opt/conan/profiles/amd64
 cmake $WORKSPACE -DCMAKE_BUILD_TYPE=Release
 make 
 
 mkdir -p $WORKSPACE/build/armhf-debug
 cd $WORKSPACE/build/armhf-debug
-conan install $WORKSPACE --build=missing --profile=$WORKSPACE/profiles/armhf
-cmake $WORKSPACE -DCMAKE_TOOLCHAIN_FILE=$WORKSPACE/armhf.cmake
+conan install $WORKSPACE --build=missing --profile=/opt/conan/profiles/armhf
+cmake $WORKSPACE -DCMAKE_TOOLCHAIN_FILE=/opt/armhf.cmake
 make 
 
 mkdir -p $WORKSPACE/build/armhf-release
 cd $WORKSPACE/build/armhf-release
-conan install $WORKSPACE --build=missing --profile=$WORKSPACE/profiles/armhf
-cmake $WORKSPACE -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$WORKSPACE/armhf.cmake
+conan install $WORKSPACE --build=missing --profile=/opt/conan/profiles/armhf
+cmake $WORKSPACE -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/opt/armhf.cmake
 make 
 
 echo 'done.'
