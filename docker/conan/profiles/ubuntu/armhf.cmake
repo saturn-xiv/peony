7bc5ebd4362af6213dd9571e092d1a66d4b4240a
Jeremy Zheng <jitang.zheng@gmail.com>
2021-03-22 17:02:00 +0000

    :wrench: using bionic

--- docker/conan/profiles/ubuntu/armhf.cmake
+++ docker/conan/profiles/ubuntu/armhf.cmake
@@ -1,7 +1,7 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_PROCESSOR arm)
 
-SET(CROSSTOOL_PREFIX /opt/toolchains/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf)
+SET(CROSSTOOL_PREFIX /opt/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf)
 # SET(CROSSTOOL_PREFIX /opt/x-tools/arm-peony-linux-gnueabi/bin/arm-peony-linux-gnueabi)
 
 SET(CMAKE_C_COMPILER   ${CROSSTOOL_PREFIX}-gcc)
