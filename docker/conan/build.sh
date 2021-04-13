e5871c9168610f7eb89740a903e0ef27ffc2cb25
Jeremy Zheng <jitang.zheng@gmail.com>
2021-04-13 13:31:03 +0000

    :bug: build on groovy

--- docker/conan/build.sh
+++ docker/conan/build.sh
@@ -14,13 +14,13 @@ mkdir -p $WORKSPACE/build/amd64-release
 cd $WORKSPACE/build/amd64-release
 conan install $CONAN_HOME --build=missing --profile=$CONAN_HOME/amd64
 cmake $WORKSPACE -DCMAKE_BUILD_TYPE=Release
-# make -j
+make -j
 
 mkdir -p $WORKSPACE/build/armhf-release
 cd $WORKSPACE/build/armhf-release
 conan install $CONAN_HOME --build=missing --profile:host=$CONAN_HOME/armhf --profile:build=$CONAN_HOME/amd64
 cmake $WORKSPACE -DCMAKE_TOOLCHAIN_FILE=$CONAN_HOME/armhf.cmake -DCMAKE_BUILD_TYPE=Release
-# make -j
+make -j
 
 
 
