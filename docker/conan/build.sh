44e7caa24dad266469c59fda1eaa6d612aa460ca
Jeremy Zheng <jitang.zheng@gmail.com>
2021-04-15 17:32:59 +0000

    :bug: set clang path in conan profile

--- docker/conan/build.sh
+++ docker/conan/build.sh
@@ -5,33 +5,32 @@ set -e
 export WORKSPACE=$PWD
 export CONAN_HOME=$WORKSPACE/docker/conan
 export CLANG_HOME="$HOME/local/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04"
-export PATH=$CLANG_HOME/bin:$PATH
-export CC=clang
-export CXX=clang++
+# export PATH=$CLANG_HOME/bin:$PATH
+# export CC=clang
+# export CXX=clang++
 
 if [ -d $WORKSPACE/build ]
 then
     rm -r $WORKSPACE/build
 fi
 
-mkdir -p $WORKSPACE/build/amd64-debug
-cd $WORKSPACE/build/amd64-debug
-conan install $CONAN_HOME --build=missing --profile=$CONAN_HOME/amd64
-# CC=gcc CXX=g++-9
-cmake $WORKSPACE -DCMAKE_BUILD_TYPE=Debug
-# make -j
+# mkdir -p $WORKSPACE/build/amd64-debug
+# cd $WORKSPACE/build/amd64-debug
+# conan install $CONAN_HOME --build=missing --profile=$CONAN_HOME/amd64
+# cmake $WORKSPACE -DCMAKE_BUILD_TYPE=Debug
+# # make -j
 
 mkdir -p $WORKSPACE/build/amd64-release
 cd $WORKSPACE/build/amd64-release
 conan install $CONAN_HOME --build=missing --profile=$CONAN_HOME/amd64
-cmake $WORKSPACE -DCMAKE_BUILD_TYPE=Release
-# # make -j
+CC=$CLANG_HOME/bin/clang CXX=$CLANG_HOME/bin/clang++ cmake $WORKSPACE -DCMAKE_BUILD_TYPE=Release
+# make -j
 
 # mkdir -p $WORKSPACE/build/armhf-release
 # cd $WORKSPACE/build/armhf-release
 # conan install $CONAN_HOME --build=missing --profile:host=$CONAN_HOME/armhf --profile:build=$CONAN_HOME/amd64
 # cmake $WORKSPACE -DCMAKE_TOOLCHAIN_FILE=$CONAN_HOME/armhf.cmake -DCMAKE_BUILD_TYPE=Release
-# # make -j
+# make -j
 
 
 
